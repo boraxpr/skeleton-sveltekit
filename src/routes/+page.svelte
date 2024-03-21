3bce53515dd765b272807fd387c987b1fe402318
boraxpr <tata-naipawat@hotmail.com>
2024-03-21 23:00:02 +0700

    Create a profile box (Flex, Flex-row, Flex-shrink-0, Flex-grow)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,24 @@
+<script lang="ts">
+	import ProfileImage from '$lib/assets/boraxprWiz.png';
+</script>
+
 <div class="container mx-auto p-8 space-y-8">
-	<h1 class="h1">Hello Skeleton</h1>
-	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-	<section>
-		<a class="btn variant-filled-primary" href="https://kit.svelte.dev/">SvelteKit</a>
-		<a class="btn variant-filled-secondary" href="https://tailwindcss.com/">Tailwind</a>
-		<a class="btn variant-filled-tertiary" href="https://github.com/">GitHub</a>
-	</section>
+	<div class="flex flex-row">
+		<div class="card flex-shrink-0">
+			<img alt="Profile" src={ProfileImage} class="border-white border-8" />
+		</div>
+		<div class="card flex-grow">
+			<header class="card-header text-2xl">Naipawat Poolsawat</header>
+			<section class="p-4">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut massa lectus. Donec
+				euismod vel magna suscipit mollis. Suspendisse potenti. Praesent faucibus malesuada ex, eu
+				finibus diam pulvinar a. Praesent venenatis ex nec sodales tincidunt. Interdum et malesuada
+				fames ac ante ipsum primis in faucibus. In lacinia lectus lorem, ac molestie ipsum elementum
+				eget. Nulla ac commodo velit. In nec ex tincidunt enim dictum tincidunt eget id metus. Fusce
+				ornare vehicula eros, eu sollicitudin eros bibendum vitae. Proin tempus blandit urna ac
+				maximus.
+			</section>
+			<footer class="card-footer">(footer)</footer>
+		</div>
+	</div>
 </div>
