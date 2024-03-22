5c16165bc2dc9f4f49516746ab1258281c2867ab
boraxpr <tata-naipawat@hotmail.com>
2024-03-23 00:52:03 +0700

    Extract image into pieces, Separate text & image, gradient border

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,33 @@
 <script lang="ts">
-	import ProfileImage from '$lib/assets/boraxprWiz.png';
+	import MainCharacter from '$lib/assets/boraxprWiz.png';
+	import Prop1 from '$lib/assets/boraxprWiz1.png';
+	import Prop2 from '$lib/assets/boraxprWiz2.png';
+	import Prop3 from '$lib/assets/boraxprWiz3.png';
+	import Prop4 from '$lib/assets/boraxprWiz4.png';
 </script>
 
-<div class="container mx-auto p-8 space-y-8">
-	<div class="flex flex-row">
-		<div class="card flex-shrink-0">
-			<img alt="Profile" src={ProfileImage} class="border-white border-8" />
+<div class="container mx-auto space-y-16 h-full flex flex-col justify-center">
+	<div class="grid grid-flow-col justify-center">
+		<div class="grid grid-flow-row">
+			<img alt="Profile" src={Prop2} class="animate-float1 row-span-1" />
+			<img alt="Profile" src={Prop4} class="animate-float2 row-span-1" />
 		</div>
-		<div class="card flex-grow">
+		<div class="grid grid-rows-1">
+			<div class="flex justify-center items-center h-full">
+				<img alt="Profile" src={MainCharacter} class="animate-float3 shrink-0" />
+			</div>
+		</div>
+		<div class="grid grid-flow-row">
+			<div>
+				<img alt="Profile" src={Prop1} class="animate-float4 row-span-1" />
+			</div>
+			<div>
+				<img alt="Profile" src={Prop3} class="animate-float5 row-span-1" />
+			</div>
+		</div>
+	</div>
+	<div class="card p-1 bg-gradient-to-r from-[#25b9bc] via-[#25ffbc] to-[#d5ffbc]">
+		<div class="bg-gray-800">
 			<header class="card-header text-2xl">Naipawat Poolsawat</header>
 			<section class="p-4">
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut massa lectus. Donec
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,42 @@ export default {
 		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
 	],
 	theme: {
-		extend: {}
+		extend: {
+			keyframes: {
+				float1: {
+					'0%': { transform: 'translateY(0)' },
+					'50%': { transform: 'translateY(-20px)' },
+					'100%': { transform: 'translateY(0)' }
+				},
+				float2: {
+					'0%': { transform: 'translateY(0)' },
+					'50%': { transform: 'translateY(15px)' },
+					'100%': { transform: 'translateY(0)' }
+				},
+				float3: {
+					'0%': { transform: 'translateY(0)' },
+					'50%': { transform: 'translateY(10px)' },
+					'100%': { transform: 'translateY(0)' }
+				},
+				float4: {
+					'0%': { transform: 'translateY(0)' },
+					'50%': { transform: 'translateY(-25px)' },
+					'100%': { transform: 'translateY(0)' }
+				},
+				float5: {
+					'0%': { transform: 'translateY(0)' },
+					'50%': { transform: 'translateY(25px)' },
+					'100%': { transform: 'translateY(0)' }
+				}
+			},
+			animation: {
+				float1: 'float1 6s ease-in-out infinite',
+				float2: 'float2 6s ease-in-out infinite',
+				float3: 'float3 6s ease-in-out infinite',
+				float4: 'float4 6s ease-in-out infinite',
+				float5: 'float5 6s ease-in-out infinite'
+			}
+		}
 	},
 	plugins: [
 		forms,
@@ -20,7 +55,7 @@ export default {
 			themes: {
 				preset: [
 					{
-						name: 'wintry',
+						name: 'vintage',
 						enhancements: true
 					}
 				]
