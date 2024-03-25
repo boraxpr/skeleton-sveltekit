06302c1c95d3e801f8871aa0f13c2d77b490ffb8
boraxpr <tata-naipawat@hotmail.com>
2024-03-25 22:22:23 +0700

    Use Lucide as nav icon & Dynamic show/hide text-icon nav bar

--- src/lib/Navigation/Navigation.svelte
+++ src/lib/Navigation/Navigation.svelte
@@ -1,6 +1,14 @@
+<script lang="ts">
+	import { Bed, ListTodo } from 'lucide-svelte';
+</script>
+
 <nav class="list-nav">
 	<ul>
-		<li><a href="/">Home</a></li>
-		<li><a href="/todo">Todo</a></li>
+		<li>
+			<a href="/"><span class="hidden lg:block">Home</span> <Bed class="lg:hidden" /></a>
+		</li>
+		<li>
+			<a href="/todo"><span class="hidden lg:block">Todo</span><ListTodo class="lg:hidden" /></a>
+		</li>
 	</ul>
 </nav>
