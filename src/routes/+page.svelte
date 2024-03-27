898bd7266d76c9d7f2ffa1716b7b6271965b49d2
boraxpr <tata-naipawat@hotmail.com>
2024-03-28 01:11:27 +0700

    Change mainpage content to include svg logos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,10 @@
 	import Prop2 from '$lib/assets/boraxprWiz2.png';
 	import Prop3 from '$lib/assets/boraxprWiz3.png';
 	import Prop4 from '$lib/assets/boraxprWiz4.png';
+	export const imageSize = {
+		height: 100,
+		width: 100
+	};
 </script>
 
 <div class="container mx-auto space-y-16 h-full flex flex-col justify-center">
@@ -28,15 +32,31 @@
 	</div>
 	<div class="card p-1 bg-gradient-to-r from-[#25b9bc] via-[#25ffbc] to-[#d5ffbc]">
 		<div class="bg-gray-800">
-			<header class="card-header text-2xl">Naipawat Poolsawat</header>
+			<header class="card-header text-2xl">Frontend</header>
 			<section class="p-4">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut massa lectus. Donec
-				euismod vel magna suscipit mollis. Suspendisse potenti. Praesent faucibus malesuada ex, eu
-				finibus diam pulvinar a. Praesent venenatis ex nec sodales tincidunt. Interdum et malesuada
-				fames ac ante ipsum primis in faucibus. In lacinia lectus lorem, ac molestie ipsum elementum
-				eget. Nulla ac commodo velit. In nec ex tincidunt enim dictum tincidunt eget id metus. Fusce
-				ornare vehicula eros, eu sollicitudin eros bibendum vitae. Proin tempus blandit urna ac
-				maximus.
+				<div class="flex flex-row">
+					<div>
+						<img
+							{...imageSize}
+							src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/svelte/svelte-original.svg"
+							alt="Svelte"
+						/>
+					</div>
+					<div>
+						<img
+							{...imageSize}
+							src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg"
+							alt="React"
+						/>
+					</div>
+					<div>
+						<img
+							{...imageSize}
+							src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-original-wordmark.svg"
+							alt="Vue"
+						/>
+					</div>
+				</div>
 			</section>
 			<footer class="card-footer">(footer)</footer>
 		</div>
