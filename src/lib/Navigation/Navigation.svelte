25566a3753dc6d404b64f32cb20daef51c3d85bc
boraxpr <tata-naipawat@hotmail.com>
2024-03-28 01:13:29 +0700

    Edit url on nav

--- src/lib/Navigation/Navigation.svelte
+++ src/lib/Navigation/Navigation.svelte
@@ -8,7 +8,8 @@
 			<a href="/"><span class="hidden lg:block">Home</span> <Bed class="lg:hidden" /></a>
 		</li>
 		<li>
-			<a href="/todo"><span class="hidden lg:block">Todo</span><ListTodo class="lg:hidden" /></a>
+			<a href="/todo_ssr"><span class="hidden lg:block">Todo</span><ListTodo class="lg:hidden" /></a
+			>
 		</li>
 	</ul>
 </nav>
