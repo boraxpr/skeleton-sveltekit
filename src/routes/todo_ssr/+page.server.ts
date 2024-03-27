ffc91e2e115b403bb7ac0e3a8416531375a34191
boraxpr <tata-naipawat@hotmail.com>
2024-03-28 01:18:27 +0700

    Note Svelte learned tips

--- src/routes/todo_ssr/+page.server.ts
+++ src/routes/todo_ssr/+page.server.ts
@@ -11,4 +11,6 @@ export const load: PageServerLoad = ({ fetch }) => {
 //  https://kit.svelte.dev/docs/load#streaming-with-promises
 // Top level promises are awaited.
 // Nested promises are streamed.
-// return promise will not be streamed instead the promise is recreated when the function reruns in the browser
+
+// !!! Top level return promise will not be streamed instead the promise is recreated when the function reruns in the browser
+// So if you return await fetch() then it will load the data on server and mimic promise on client (It's not lazy load and make the page load super long)
