0b1ea89d04b75aa2b5b3705d6739a8e506bc6190
boraxpr <tata-naipawat@hotmail.com>
2024-03-28 21:10:13 +0700

    Add CSR Page /w placeholder api endpoint

---
+++ src/routes/todo/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { ProgressRadial } from '@skeletonlabs/skeleton';
+	import MyTable from '$lib/Table.svelte';
+	let isLoading = false;
+
+	let data: { name: string; email: string; phone: string; website: string }[] = [];
+	onMount(async () => {
+		isLoading = true;
+		const response = await fetch('https://jsonplaceholder.typicode.com/users');
+		data = await response.json();
+		isLoading = false;
+	});
+</script>
+
+<div class="card">
+	{#if isLoading}
+		<div class="w-full">
+			<ProgressRadial meter="stroke-primary-500" />
+		</div>
+	{:else if data}
+		<table>
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Email</th>
+					<th>Phone</th>
+					<th>Website</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#await data then data}
+					{#each data as user}
+						<tr>
+							<td>{user.name}</td>
+							<td>{user.email}</td>
+							<td>{user.phone}</td>
+							<td>{user.website}</td>
+						</tr>
+					{/each}
+				{/await}
+			</tbody>
+		</table>
+	{:else}
+		<p>No data available yet.</p>
+	{/if}
+</div>
