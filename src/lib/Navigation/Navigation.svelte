75c733bd57efc8ef9242a7aad5b766d5eb594e0b
boraxpr <tata-naipawat@hotmail.com>
2024-03-28 21:10:23 +0700

    Add new link to Navigation

--- src/lib/Navigation/Navigation.svelte
+++ src/lib/Navigation/Navigation.svelte
@@ -8,7 +8,12 @@
 			<a href="/"><span class="hidden lg:block">Home</span> <Bed class="lg:hidden" /></a>
 		</li>
 		<li>
-			<a href="/todo_ssr"><span class="hidden lg:block">Todo</span><ListTodo class="lg:hidden" /></a
+			<a href="/todo_ssr"
+				><span class="hidden lg:block">Todo SSR</span><ListTodo class="lg:hidden" /></a
+			>
+		</li>
+		<li>
+			<a href="/todo"><span class="hidden lg:block">Todo CSR</span><ListTodo class="lg:hidden" /></a
 			>
 		</li>
 	</ul>
